f18b5846f6e0a908a584f1759fb0b3adff8d2ae1
Raquel Ventura <rag35540@gmail.com>
2024-11-03 14:14:21 -0600

    corrigiendo secciones

--- database/seeders/SeccionSeeder.php
+++ database/seeders/SeccionSeeder.php
@@ -17,99 +17,80 @@ class SeccionSeeder extends Seeder
     {
     Seccion::factory()->create([
         'nombre_seccion' => 'Administración de Empresas',
-        'id_departamento' => '',
+        'id_departamento' => 2,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Agronomía',
-        'id_departamento' => '',
+        'id_departamento' => 1,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Arquitectura',
-        'id_departamento' => '',
+        'id_departamento' => 4,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Biología',
-        'id_departamento' => '',
+        'id_departamento' => 7,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Ciencias Químicas',
-        'id_departamento' => '',
+        'id_departamento' => 7,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Contaduría pública',
-        'id_departamento' => '',
-    ]);
-    Seccion::factory()->create([
-        'nombre_seccion' => 'Doctorado en Medicina',
-        'id_departamento' => '',
+        'id_departamento' => 2,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Educación',
-        'id_departamento' => '',
-    ]);
-    Seccion::factory()->create([
-        'nombre_seccion' => 'Escuela de Carreras Técnicas de Morazán',
-        'id_departamento' => '',
+        'id_departamento' => 3,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Física',
-        'id_departamento' => '',
+        'id_departamento' => 7,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Idiomas',
-        'id_departamento' => '',
+        'id_departamento' => 3,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Ingeniería Civil',
-        'id_departamento' => '',
+        'id_departamento' => 4,
     ]);    
     Seccion::factory()->create([
         'nombre_seccion' => 'Ingeniería de Sistemas Informáticos',
-        'id_departamento' => '',
+        'id_departamento' => 4,
     ]);    
     Seccion::factory()->create([
         'nombre_seccion' => 'Ingeniería Industrial',
-        'id_departamento' => '',
+        'id_departamento' => 4,
     ]);    
     Seccion::factory()->create([
         'nombre_seccion' => 'Jurisprudencia y Ciencias Sociales',
-        'id_departamento' => '',
+        'id_departamento' => 5,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Letras',
-        'id_departamento' => '',
-    ]);
-    Seccion::factory()->create([
-        'nombre_seccion' => 'Lic. en Anestosiología e Inhaloterapia',
-        'id_departamento' => '',
-    ]);
-    Seccion::factory()->create([
-        'nombre_seccion' => 'Lic. en Fisioterapia y Terapia Ocupacional',
-        'id_departamento' => '',
+        'id_departamento' => 3,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Lic. en Laboratorio Clínico',
-        'id_departamento' => '',
+        'id_departamento' => 6,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Matemática',
-        'id_departamento' => '',
+        'id_departamento' => 7,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Mercadeo Internacional',
-        'id_departamento' => '',
+        'id_departamento' => 2,
     ]);
-    Seccion::factory()->create([
-        'nombre_seccion' => 'Post-Grado',
-        'id_departamento' => '',
-    ]);
+    
     Seccion::factory()->create([
         'nombre_seccion' => 'Psicología',
-        'id_departamento' => '',
+        'id_departamento' => 3,
     ]);
     Seccion::factory()->create([
         'nombre_seccion' => 'Sociología',
-        'id_departamento' => '',
+        'id_departamento' => 3,
     ]);
 
     }
