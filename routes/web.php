9af37d5b82706d8ab35eac34a4a87145ca667805
William Vásquez <vf18003@ues.edu.sv>
2024-11-03 17:30:22 -0600

    endpoint de estados y de documentos

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentoController;
+use Illuminate\Http\Request;
+
 
 Route::get('/', function () {
     return view('login.login');
 });
-
-Route::get('/permisos', function () {
-    return view('permisos.gestionpermiso');
-})
-->name('permisos');
 
 Route::get('/registro', function () {
     return view('registro.registro');
@@ -31,7 +29,68 @@ Route::get('/usuarios', function () {
     return view('usuarios.listaUsuario');
 })->name('usuarios');
 
-Route::get('/perfil', function () {
-    return view('perfil.perfilUsuario');
-})
-->name('perfil');
+
+//endpoint de documentos 
+Route::prefix('documentos')->group(function () {
+    Route::get('/', function () {
+        $controller = app(DocumentoController::class);
+        $response = $controller->index();
+        return response()->json($response->getData()); 
+    });
+
+    Route::post('/', function (Request $request) {
+        $controller = app(DocumentoController::class);
+        $controller->store($request);
+        return response()->json(['message' => 'Documento creado exitosamente']);
+    });
+
+    Route::get('/{id}', function ($id) {
+        $controller = app(DocumentoController::class);
+        $response = $controller->show($id);
+        return response()->json($response->getData());
+    });
+
+    Route::put('/{id}', function (Request $request, $id) {
+        $controller = app(DocumentoController::class);
+        $controller->update($request, $id);
+        return response()->json(['message' => 'Documento actualizado exitosamente']);
+    });
+
+    Route::delete('/{id}', function ($id) {
+        $controller = app(DocumentoController::class);
+        $controller->destroy($id);
+        return response()->json(['message' => 'Documento eliminado exitosamente']);
+    });
+});
+//endpoint de estados
+Route::prefix('estados')->group(function () {
+    Route::get('/', function () {
+        $controller = app(EstadoController::class);
+        $response = $controller->index();
+        return response()->json($response->getData()); 
+    });
+
+    Route::post('/', function (Request $request) {
+        $controller = app(EstadoController::class);
+        $controller->store($request);
+        return response()->json(['message' => 'Estado creado exitosamente']);
+    });
+
+    Route::get('/{id}', function ($id) {
+        $controller = app(EstadoController::class);
+        $response = $controller->show($id);
+        return response()->json($response->getData());
+    });
+
+    Route::put('/{id}', function (Request $request, $id) {
+        $controller = app(EstadoController::class);
+        $controller->update($request, $id);
+        return response()->json(['message' => 'Estado actualizado exitosamente']);
+    });
+
+    Route::delete('/{id}', function ($id) {
+        $controller = app(EstadoController::class);
+        $controller->destroy($id);
+        return response()->json(['message' => 'Estado eliminado exitosamente']);
+    });
+});
