3f6b336aba322221de4eee2a90e4eec3d758d00d
yoselinuesfom <jo21003@ues.edu.sv>
2024-11-10 16:42:59 -0600

    actualizacion en seederAsignaciones

--- database/seeders/AsignacionesSeeder.php
+++ database/seeders/AsignacionesSeeder.php
@@ -19,24 +19,25 @@ class AsignacionesSeeder extends Seeder
      */
     public function run(): void
     {
-        // Obtener todos los estudiantes, proyectos y tutores
-        $estudiantes = Estudiante::all();
-        $proyectos = Proyecto::all();
-        $tutores = User::role('Tutor')->get();
-        
-        // Verificar si hay datos en las tablas necesarias
-        if ($estudiantes->isEmpty() || $proyectos->isEmpty() || $tutores->isEmpty()) {
-            throw new \Exception('Probablemente no hay datos de Estudiantes, datos de Proyectos o datos de Tutor');
-        }
-
-        // Iterar sobre los estudiantes y asignarles un proyecto y tutor aleatorio
-        foreach ($estudiantes as $estudiante) {
-            Asignacion::create([
-                'id_proyecto' => $proyectos->random()->id_proyecto, // Asumiendo que la columna se llama id_proyecto
-                'id_estudiante' => $estudiante->id_estudiante, // Asumiendo que la columna se llama id_estudiante
-                'id_tutor' => $tutores->random()->id_usuario, // Asumiendo que la columna se llama id_usuario
-                'fecha_asignacion' => Carbon::now()->subDays(rand(0, 365))->format('Y-m-d'), // Cambiado el formato de fecha
-            ]);
-        }
-    }
+         // Obtener todos los estudiantes, proyectos y tutores
+         $estudiantes = Estudiante::all();
+         $proyectos = Proyecto::all();
+         $tutores = User::role('Tutor')->get(); // Obtener solo los usuarios con el rol de 'Tutor'
+         
+         // Verificar si hay datos en las tablas necesarias
+         if ($estudiantes->isEmpty() || $proyectos->isEmpty() || $tutores->isEmpty()) {
+             throw new \Exception('Probablemenete no hay datos de Estudiantes, datos de Proyectos o datos de Tutor');
+         }
+         // Iterar sobre los estudiantes y asignarles un proyecto y tutor aleatorio
+         foreach ($estudiantes as $estudiante) {
+             Asignacion::create([
+                 'id_proyecto' => $proyectos->random()->id, // Selecciona un proyecto aleatorio
+                 'id_estudiante' => $estudiante->id, // ID del estudiante actual
+                 'id_tutor' => $tutores->random()->id, // Selecciona un tutor aleatorio
+                 'fecha_asignacion' => Carbon::now()->subDays(rand(0, 365))->format('d/m/Y'), // Fecha aleatoria en el ultimo año con formato dia/mes/año
+             ]);
+         }
+          
+     }
+    
 }
