fcbfbadeb4686d17e078948a7fc4be99304de0b7
Meybell25 <meybell01quintanilla@gmail.com>
2024-11-10 19:59:04 -0600

    Instalando y configurando JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+class User extends Authenticatable Implements JWTSubject 
 {
     use HasRoles;
     use Notifiable;
@@ -33,5 +35,25 @@ class User extends Authenticatable
             'id_tutor',
             'id_seccion'
         );
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
