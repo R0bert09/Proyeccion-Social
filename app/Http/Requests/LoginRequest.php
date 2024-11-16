e0641a9fab0b46c5c76692aeed89511355ee9029
Angel Ramirez <119237530+AngelRamirez18@users.noreply.github.com>
2024-11-15 18:18:22 -0600

    creacion request clases para validaciones
    **Controlador de validacion de datos en el registro y login
    
    **Creacion de RegisterRequest y LoginRequest para la validacion

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    
+    public function authorize(): bool
+    {
+        return true; 
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ];
+    }
+
+    /**
+     * Get the custom messages for validation errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'El correo electrónico es obligatorio.',
+            'email.email' => 'Debes proporcionar un correo electrónico válido.',
+            'password.required' => 'La contraseña es obligatoria.',
+        ];
+    }
+}
